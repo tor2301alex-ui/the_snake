09353bdd9219c805bd04bb21b3d41478099656c0
Торопина Полина <tor.2301alex@gmail.com>
2026-04-23 20:52:32 +0300

    add new class Apple

--- the_snake.py
+++ the_snake.py
@@ -63,9 +63,42 @@ class GameObject:
         """
         pass
         
+class Apple(GameObject):
+    """
+    Класс Apple. Наследуется от GameObject.
+    Появляется в случайном месте поля.
+    """
+    apple_color = (255, 0, 0)
 
-def main():
-    # Инициализация PyGame:
+    super().__init__(position=None, body_color=apple_color)
+
+    self.randomize_position()
+
+    def randomize_position(self):
+        """
+        Устанавливает случайные координаты для яблока.
+        """
+        max_x = 640 - 20
+        max_y = 480 - 20
+
+        x.random.randrage(0, max_x + 1, 20)
+        y.random.randrage(0, max_y + 1, 20)
+
+        self.position = (x, y)
+
+    def draw(self, surface):
+        """
+        Отрисовывает яблоко на игровом поле.
+        """
+
+        rect = pygame.Rect(
+            self.position[0],
+            self.position[1],
+            20,
+            20
+        )    
+
+        pygame.draw.rect(surface, self.body_color, rect)
     pygame.init()
     # Тут нужно создать экземпляры классов.
     ...
