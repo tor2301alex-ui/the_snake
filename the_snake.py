fb9688afb0820d67a0ba25329864d7267d89f5a5
Polina Toropina <tor2301alex@gmail.com>
2026-04-23 16:57:35 +0300

    add new class GameObject

--- the_snake.py
+++ the_snake.py
@@ -40,8 +40,29 @@ clock = pygame.time.Clock()
 
 
 # Тут опишите все классы игры.
-...
-
+class GameObject:
+    """
+    Базовый класс, от которого наследуются все объекты.
+    Содержит общие атрибуты: позиция и цвет.
+    """
+    
+    def __init__(self, position=None, body_color=None):
+        """
+        Конструктор базового игрового объекта.
+        Аргументы: position (координаты), body_color (цвет).
+        """
+        if position is None:
+            self.position = (320, 240)
+        else:
+            self.position = position
+    
+    def draw(self, surface):
+        """
+        Абстрактный метод отрисовки объекта на экране.
+        Аргумент: surface (поверхность, на которой рисуем)
+        """
+        pass
+        
 
 def main():
     # Инициализация PyGame:
