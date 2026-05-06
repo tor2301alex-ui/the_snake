6233b0f83b55b439b2c8ed1d82b8b1cbf8a73b72
Торопина Полина <tor.2301alex@gmail.com>
2026-05-07 00:00:57 +0300

    привела в соотвествие с PEP8

--- the_snake.py
+++ the_snake.py
@@ -39,61 +39,39 @@ pygame.display.set_caption('Змейка')
 clock = pygame.time.Clock()
 
 
-# Тут опишите все классы игры.
 class GameObject:
-    """
-    Базовый класс, от которого наследуются все объекты.
-    Содержит общие атрибуты: позиция и цвет.
-    """
-    
+    """Базовый класс для всех игровых объектов."""
+
     def __init__(self, position=None, body_color=None):
-        """
-        Конструктор базового игрового объекта.
-        Аргументы: position (координаты), body_color (цвет).
-        """
-        if position is None:
-            self.position = (320, 240)
-        else:
-            self.position = position
-    
+        """Инициализирует базовый игровой объект."""
+        self.position = position
+        self.body_color = body_color
+
     def draw(self, surface):
-        """
-        Абстрактный метод отрисовки объекта на экране.
-        Аргумент: surface (поверхность, на которой рисуем)
-        """
+        """Абстрактный метод для отрисовки объекта."""
         pass
-        
+
+
 class Apple(GameObject):
-    """
-    Класс Apple. Наследуется от GameObject.
-    Появляется в случайном месте поля.
-    """
-    
+    """Класс яблока, наследуется от GameObject."""
+
     def __init__(self):
         """Инициализирует яблоко со случайной позицией."""
         super().__init__(body_color=APPLE_COLOR)
         self.randomize_position()
 
     def randomize_position(self):
-        """
-        Устанавливает случайные координаты для яблока.
-        """
-        max_x = 640 - 20
-        max_y = 480 - 20
-
-        x.random.randrage(0, max_x + 1, 20)
-        y.random.randrage(0, max_y + 1, 20)
-
+        """Устанавливает случайную позицию яблока на игровом поле."""
+        x = randint(0, GRID_WIDTH - 1) * GRID_SIZE
+        y = randint(0, GRID_HEIGHT - 1) * GRID_SIZE
         self.position = (x, y)
 
     def draw(self, surface):
-        """
-        Отрисовывает яблоко на игровом поле.
-        """
+        """Отрисовывает яблоко на игровой поверхности."""
         rect = pygame.Rect(self.position, (GRID_SIZE, GRID_SIZE))
         pygame.draw.rect(surface, self.body_color, rect)
         pygame.draw.rect(surface, BORDER_COLOR, rect, 1)
-        
+
 
 class Snake(GameObject):
     """Класс змейки, наследуется от GameObject."""
@@ -119,7 +97,8 @@ class Snake(GameObject):
         self.positions = [(center_x, center_y)]
         self.length = 1
         self.last = None
-     def update_direction(self):
+
+    def update_direction(self):
         """Обновляет направление движения змейки."""
         if self.next_direction:
             self.direction = self.next_direction
@@ -144,7 +123,7 @@ class Snake(GameObject):
         else:
             self.last = None
 
-     def draw(self, surface):
+    def draw(self, surface):
         """Отрисовывает змейку на игровой поверхности."""
         for position in self.positions[:-1]:
             rect = pygame.Rect(position, (GRID_SIZE, GRID_SIZE))
