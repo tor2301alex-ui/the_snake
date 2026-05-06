a09bdde10f941c50369d773976e2243baed97584
Торопина Полина <tor.2301alex@gmail.com>
2026-05-06 23:53:23 +0300

    отрисовка змейки

--- the_snake.py
+++ the_snake.py
@@ -143,6 +143,22 @@ class Snake(GameObject):
             self.last = self.positions.pop()
         else:
             self.last = None
+
+     def draw(self, surface):
+        """Отрисовывает змейку на игровой поверхности."""
+        for position in self.positions[:-1]:
+            rect = pygame.Rect(position, (GRID_SIZE, GRID_SIZE))
+            pygame.draw.rect(surface, self.body_color, rect)
+            pygame.draw.rect(surface, BORDER_COLOR, rect, 1)
+
+        head_rect = pygame.Rect(self.positions[0], (GRID_SIZE, GRID_SIZE))
+        pygame.draw.rect(surface, self.body_color, head_rect)
+        pygame.draw.rect(surface, BORDER_COLOR, head_rect, 1)
+
+        if self.last:
+            last_rect = pygame.Rect(self.last, (GRID_SIZE, GRID_SIZE))
+            pygame.draw.rect(surface, BOARD_BACKGROUND_COLOR, last_rect)
+
     
     # Тут нужно создать экземпляры классов.
     ...
